5f78f3a44c6940534ee63ce3995882f3a71b2fe5
VadimSchmitz <vadimschmitz99@gmail.com>
2020-10-12 20:44:24 +0200

    update

--- Client/src/Components/Header/Header.js
+++ Client/src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Header.scss';
 import Bannerimage from "../../svg/Bannerimage.svg"
 import Wave from "../../svg/Wave.svg"
+import BannerBackground from "../../svg/BannerBackground.svg"
 
 export default function Header() {
   return (
